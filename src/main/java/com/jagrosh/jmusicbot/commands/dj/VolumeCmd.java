4ae3d619daf90f5a0f26aa63c96d479a7007bfe4
Wapitiii <39195287+Wapitiii@users.noreply.github.com>
2020-11-06 15:37:09 +0100

    earrape modification

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/VolumeCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/VolumeCmd.java
@@ -55,7 +55,7 @@ public class VolumeCmd extends DJCommand
             }catch(NumberFormatException e){
                 nvolume = -1;
             }
-            if(nvolume<0 || nvolume>150)
+            if(nvolume<0 || nvolume>2147483647)
                 event.reply(event.getClient().getError()+" Volume must be a valid integer between 0 and 150!");
             else
             {
